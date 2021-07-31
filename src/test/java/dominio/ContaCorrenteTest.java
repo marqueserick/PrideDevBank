da1695be88ae975f5db52d4e467d0bee7ca122f6
Erick Marques <erickmarques43@gmail.com>
2021-07-31 00:31:51 -0300

    aumento cobertura de test

--- src/test/java/dominio/ContaCorrenteTest.java
+++ src/test/java/dominio/ContaCorrenteTest.java
@@ -1,18 +1,26 @@
 package dominio;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class ContaCorrenteTest {
+	ContaCorrente conta;
+	
+	@BeforeEach
+	void init() {
+		conta = new ContaCorrente();
+		conta.depositar(100);
+	}
 	
 	@Test
-	void deveSerpossivelSacarUtilizandoChequeEspecial() {
+	void deveSerPossivelSacarUtilizandoChequeEspecial() {
 		//preparacao
-		ContaCorrente conta = new ContaCorrente();
-//		execucao
-		conta.depositar(100);
+		//execucao
 		conta.sacar(176);
 		//validacao
 		assertTrue(conta.getSaldo() == -76);
@@ -21,9 +29,14 @@ public class ContaCorrenteTest {
 	
 	@Test
 	void naoDeveSerPossivelSacarAlemDoChequeEspecial() {
-		
 		ContaCorrente conta = new ContaCorrente();
-		assertThrows(RuntimeException.class,()-> conta.sacar(200));
+		assertThrows(RuntimeException.class,()-> conta.sacar(201));
 		}
+	
+	@Test
+	void deveSerPossivelSacarComSaldo() {
+		conta.sacar(99);
+		assertTrue(conta.getSaldo()==1);
+	}
 
 }
